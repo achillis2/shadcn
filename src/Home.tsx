d99b260b27363069297b52859ecdb7c2fa65e5e4
Ding Li <dli@resource-innovations.com>
2023-09-26 13:27:14 -0700

    add tab, cards, label, button etc.

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import TabsDemo from "./TabsDemo";
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <TabsDemo />
+      {/* <Button>Click me</Button> */}
     </div>
   );
 }
